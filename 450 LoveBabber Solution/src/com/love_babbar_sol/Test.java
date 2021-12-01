67fe290cb1c129a6dd1506a66c05f1f3e21e1895
jbhan <jbhan@DESKTOP-CC5IL9H>
2021-12-01 19:55:28 +0530

    FIRST COMMIT

---
+++ 450 LoveBabber Solution/src/com/love_babbar_sol/Test.java
@@ -0,0 +1,10 @@
+package com.love_babbar_sol;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
